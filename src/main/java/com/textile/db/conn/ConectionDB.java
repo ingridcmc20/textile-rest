d715c6960fca58cfbd3d3d5cec1be949cdaba83b
Ingrid <ingridmendoza86@gmail.com>
2018-11-29 23:46:13 -0500

    BD

--- src/main/java/com/textile/db/conn/ConectionDB.java
+++ src/main/java/com/textile/db/conn/ConectionDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConectionDB {
 	// static String url = "jdbc:postgresql://adielh.com:5432/adielh_victorian";
-	// static String usuario = "adielh_root";
+	// static String usuario = "adielh_root"; 
 	// static String password = "$4dm1nr00t$";
 	static String url = "jdbc:postgresql://ec2-54-163-230-178.compute-1.amazonaws.com:5432/d52q7qdi1ne7fp?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 	static String usuario = "whhsgcvcwqgfhe";
